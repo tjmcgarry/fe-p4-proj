f5da708c04a585d2ce67989dcbb44e3caae61eeb
Thomas McGarry <tjmcgarryiv@gmail.com>
2022-01-26 09:34:20 -0600

    books create in books controller, added ten seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
 User.create(username: "Sam", email: "samatgoogle")
 
 Book.create(author: "Mitch Albom", title: "THE STRANGER IN THE LIFEBOAT", img_url: "https://storage.googleapis.com/du-prd/books/images/9780062888341.jpg" )
+
 Book.create(author: "Michael Connelly", title: "THE DARK HOURS", img_url: "https://storage.googleapis.com/du-prd/books/images/9780316485647.jpg")
+
 Book.create(author: "John Grisham", title: "THE JUDGE'S LIST", img_url: "https://storage.googleapis.com/du-prd/books/images/9780385546027.jpg" )
+
 Book.create(author: "Nicholas Sparks", title: "THE WISH", img_url: "https://storage.googleapis.com/du-prd/books/images/9781538728628.jpg" )
-Book.create(author: "Ken Follett", title: "NEVER", img_url: "https://storage.googleapis.com/du-prd/books/images/9780593300015.jpg")
+
+Book.create(author: "Lee Child and Andrew Child", title: "BETTER OFF DEAD", img_url: "https://storage.googleapis.com/du-prd/books/images/9781984818508.jpg")
+
+Book.create(author: "Janet Evanovich", title: "GAME ON", img_url: "https://storage.googleapis.com/du-prd/books/images/9781982154875.jpg")
+
+Book.create(author: "Amor Towles", title: "THE LINCOLN HIGHWAY", img_url: "https://storage.googleapis.com/du-prd/books/images/9780735222359.jpg")
+
+Book.create(author: "Louise Erdrich", title: "THE SENTENCE", img_url: "https://storage.googleapis.com/du-prd/books/images/9780062671127.jpg")
+
+Book.create(author: "Stephen King", title: "BILLY SUMMERS", img_url: "https://storage.googleapis.com/du-prd/books/images/9781982173616.jpg")
+
+Book.create(author: "Anthony Doerr", title: "CLOUD CUCKOO LAND", img_url: "https://storage.googleapis.com/du-prd/books/images/9781982168438.jpg")
+
+Book.create(author: "Matt Haig", title: "THE MIDNIGHT LIBRARY", img_url: "https://storage.googleapis.com/du-prd/books/images/9780525559474.jpg")
+
+Book.create(author: "Laura Dave", title: "THE LAST THING HE TOLD ME", img_url: "https://storage.googleapis.com/du-prd/books/images/9781501171345.jpg")
+
+Book.create(author: "Liane Moriarty", title: "APPLES NEVER FALL", img_url: "https://storage.googleapis.com/du-prd/books/images/9781250220257.jpg")
+
+Book.create(author: "Hilary Rodham Clinton and Louise Penny", title: "STATE OF TERROR", img_url: "https://storage.googleapis.com/du-prd/books/images/9781982173678.jpg")
